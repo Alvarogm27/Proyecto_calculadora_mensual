9033a4b3ad09715ed8ee7af869b25835ec5dc4f2
Alvarogm27 <alvarogarciamarquez.ccg@gmail.com>
2024-11-19 20:49:55 +0100

    Final Proyecto finalizado

---
+++ calculation.php
@@ -0,0 +1,44 @@
+<?php
+$ingresos = $_POST['ingresos'];
+$alquiler = $_POST['alquiler'];
+$comida = $_POST['comida'];
+$transporte = $_POST['transporte'];
+$entretenimiento = $_POST['entretenimiento'];
+
+$gastos = $alquiler + $comida + $transporte + $entretenimiento;
+$diferencia = $ingresos - $gastos;
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Resultado</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php if ($ingresos < $gastos): ?>
+        <h1>CUIDADO PODRÍAS ESTAR EN BANCA ROTA</h1>
+        <h2>Desglose de gastos</h2>
+        <ul>
+            <li>En alquiler has gastado: <?= $alquiler ?> €</li>
+            <li>En comida has gastado: <?= $comida ?> €</li>
+            <li>En transporte has gastado: <?= $transporte ?> €</li>
+            <li>En entretenimiento has gastado: <?= $entretenimiento ?> €</li>
+        </ul>
+        <p>Estás gastando un total de <?= $gastos ?> €, lo cual excede tus ingresos.</p>
+    <?php else: ?>
+        <h1>¡MUY BIEN, SIGUE ASÍ!</h1>
+        <h2>Desglose de gastos</h2>
+        <ul>
+            <li>En alquiler has gastado: <?= $alquiler ?> €</li>
+            <li>En comida has gastado: <?= $comida ?> €</li>
+            <li>En transporte has gastado: <?= $transporte ?> €</li>
+            <li>En entretenimiento has gastado: <?= $entretenimiento ?> €</li>
+        </ul>
+        <p>Estás gastando un total de <?= $gastos ?> €, ahorrando <?= $diferencia ?> € cada mes. ¡Sigue así!</p>
+    <?php endif; ?>
+</body>
+</html>
